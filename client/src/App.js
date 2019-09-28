c744ce221ede4ceb337dd968a5e11d5142c35592
Marshall Bowles <marshall@marshallbowles.com>
2019-09-28 14:11:01 -0700

    Pull image urls from server on interval

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,40 @@ import './App.css';
 import Image from './media/Image.js';
 import Loader from './media/Loader.js';
 
-function App() {
-  const imageUrl = Loader.getImageUrl();
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Image image={imageUrl} className='alpha' />
-      </header>
-    </div>
-  );
+const interval = 5000;
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { imageUrl: 'test' };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      Loader.getImageUrl()
+        .then((response) => {
+          console.log(response.data);
+          this.setState({
+            imageUrl: response.data,
+          });
+        });
+    }, interval);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Image image={this.state.imageUrl} className='alpha' />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- server/index.ts
+++ server/index.ts
@@ -2,9 +2,26 @@ import {Request, Response} from 'express';
 import * as express from 'express';
 
 const app = express();
+const cors = require('cors');
+
+const PORT = 3030;
+
+//const testImage = 'images/dots-3x4.png';
+const images = [
+  'blue-3x4.png',
+  'blue-square.png',
+  'dots-3x4.png',
+  'orange-square.png',
+  'pink-4x3.png',
+  'striped-4x3.png',
+];
+
+app.use(cors());
+app.use(express.static(__dirname + '/assets'));
  
-app.get('/', function (_req: Request, res: Response) {
-  res.send('Hello World');
+app.get('/', function (req: Request, res: Response) {
+  const index = Math.floor(Math.random() * images.length);
+  res.send(`${req.headers.host}/images/${images[index]}`);
 });
  
-app.listen(3000);
+app.listen(PORT);
