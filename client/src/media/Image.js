810da4343a21d8fe4e7452f16f7089cbd6a973c3
Marshall Bowles <marshall@marshallbowles.com>
2019-09-29 13:58:55 -0700

    Add optional style override for image

--- client/src/media/Image.js
+++ client/src/media/Image.js
@@ -2,12 +2,18 @@ import React from 'react';
 import './Image.css';
 import './animation.css';
 
+/**
+ * Render and image.
+ * @param props.className CSS class name.
+ * @param props.image Image URL.
+ * @param props.style Optional style overrides.
+ */
 function Image(props) {
-  console.log(props.image);
   return (
     <img
       className={props.className}
       src={props.image}
+      style={props.style}
       alt={props.image} />
   );
 }
