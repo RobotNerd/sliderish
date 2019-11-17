619a9f521db7c7f162d65464740ad28db0caac34
Marshall Bowles <marshall@marshallbowles.com>
2019-11-16 18:56:43 -0800

    Refactor server code

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,5 @@
 import {Request, Response} from 'express';
+import {ImagePicker} from './src/image';
 import * as cors from 'cors';
 import * as express from 'express';
 import * as sanitize from './src/sanitize';
@@ -9,45 +10,14 @@ const PATHS = [
   `${__dirname}/assets/alaska`,
   `${__dirname}/assets/pics`,
 ];
-
-import { FilePicker } from './src/file-picker';
-import { PathOrder } from './src/path-order';
-const order = new PathOrder(PATHS);
-
-
-const cycleLimit: number = 10;
-var shown: number = 0;
-var basePath = order.getNext();
-var picker = new FilePicker(order.getNext());
-function getImage(host: string, count: number = 1): string[] {
-  if (shown > cycleLimit) {
-    shown = 0;
-    basePath = order.getNext();
-    picker = new FilePicker(basePath);
-  }
-  shown += count;
-  const imagePaths = [];
-  for (let imagePath of picker.getNext(count)) {
-    if (imagePath) {
-      imagePaths.push(
-        `http://${host}/` +
-        imagePath.substring(__dirname.length + '/assets/'.length)
-      );
-    }
-    else {
-      console.log("WARNING: imagePath is undefined or empty string");
-      console.log(imagePaths);
-    }
-  };
-  return imagePaths;
-}
+const imagePicker = new ImagePicker(PATHS, __dirname + '/assets/');
 
 app.use(cors());
 app.use(express.static(__dirname + '/assets'));
 
 app.get('/', function (req: Request, res: Response) {
   const count = sanitize.requestedCount(req);
-  res.send(getImage(req.headers.host as string, count));
+  res.send(imagePicker.getImages(req.headers.host as string, count));
 });
 
 app.listen(PORT);
---
+++ server/src/image.ts
@@ -0,0 +1,46 @@
+import { FilePicker } from './file-picker';
+import { PathOrder } from './path-order';
+
+const CYCLE_LIMIT = 10;
+
+
+export class ImagePicker {
+
+  filePicker!: FilePicker;
+  pathOrder!: PathOrder;
+  basePath!: string;     // TODO make specific to client making the request
+  shown!: number;        // TODO make specific to client making the request
+
+  constructor(rootPaths: string[], private prefixPath: string) {
+    this.pathOrder = new PathOrder(rootPaths);
+    this.changeSource();
+  }
+
+  /** Get images to be served to client. */
+  getImages(host: string, count: number = 1): string[] {
+    if (this.shown > CYCLE_LIMIT) {
+      this.changeSource();
+    }
+    this.shown += count;
+    const imagePaths = [];
+    for (let imagePath of this.filePicker.getNext(count)) {
+      if (imagePath) {
+        imagePaths.push(
+          `http://${host}/` +
+          imagePath.substring(this.prefixPath.length)
+        );
+      }
+      else {
+        console.log("WARNING: imagePath is undefined or empty string");
+        console.log(imagePaths);
+      }
+    };
+    return imagePaths;
+  }
+
+  private changeSource() {
+    this.shown = 0;
+    this.basePath = this.pathOrder.getNext();
+    this.filePicker = new FilePicker(this.basePath);
+  }
+}
